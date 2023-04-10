8b279c6c4030fc5991d7756328e737317b9af060
victordnb <victordnb@gmail.com>
2023-04-10 09:50:50 +0200

    introduccion estilos

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MiComponente></MiComponente>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MiComponente from './components/MiComponente.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MiComponente
+
   }
 }
 </script>
---
+++ src/components/MiComponente.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="mi-componente">
+        <hr/>
+        <h1>Este es MiComponente</h1>
+        <h2>{{ subtitulo }}</h2>
+        <hr/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MiComponente',
+    data(){
+        return {
+            subtitulo: 'Soy el subtitulo de mi componente.'
+        }
+    }
+}
+</script>
